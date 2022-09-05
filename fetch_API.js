c183c72d02ffdec17f6ca940e586f5b54e15d531
funkkphil3000 <109705563+funkkphil3000@users.noreply.github.com>
2022-09-05 16:32:12 -0400

    Add files via upload

---
+++ fetch_API.js
@@ -0,0 +1,32 @@
+//fetch('https://www.theweathernetwork.com/ca')
+const catResult = document.getElementById('catResult');
+const dogResult = document.getElementById('dogResult');
+const catBtn = document.getElementById('catBtn');
+const dogBtn = document.getElementById('dogBtn');
+catBtn.addEventListener('click',getRandomCat)
+dogBtn.addEventListener('click',getRandomDog)
+function getRandomCat(){
+    fetch('https://aws.random.cat/meow')
+    .then(res=>res.json())
+    .then(data=>{
+        catResult.innerHTML = `<img src="${data.file}"/>`
+        console.log(data)
+    })
+}
+function getRandomDog(){
+    fetch('https://random.dog/woof.json')
+    .then (res=>res.json())
+    .then(data=>{
+        if(data.url.includes('.mp4')){
+            getRandomDog()
+        }else{
+            dogResult.innerHTML = `<img src ="${data.url}"/>`
+        }
+    })
+}
+
+
+
+
+
+
